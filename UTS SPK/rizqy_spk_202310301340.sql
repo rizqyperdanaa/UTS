3772d48f2aeb76b481dc5748e1ebb55049ef2a9c
rizqyperdanaa <149360809+rizqyperdanaa@users.noreply.github.com>
2023-10-30 13:59:43 +0700

    Add files via upload

---
+++ UTS SPK/rizqy_spk_202310301340.sql
@@ -0,0 +1,11 @@
+INSERT INTO rizqy_spk (nama_hp,reputasi_brand,processor_antutu,baterai,harga,ukuran_layar) VALUES
+	 ('Realme 10 Pro 5G','Lumayan Terkenal',424500,'5000 mAh','4,3 jutaan',6.72),
+	 ('Samsung Galaxy A34','Terkenal',538000,'5000 mAh','5 jutaan',6.60),
+	 ('Samsung Galaxy A33','Terkenal',415448,'5000 mAh','4,5 jutaan',6.40),
+	 ('Oppo Reno 8 5G','Cukup Terkenal',402916,'4800 mAh','4,4 jutaan',6.70),
+	 ('Vivo T1 Pro 5G','Cukup Terkenal',568448,'4700 mAh','4,5 jutaan',6.44),
+	 ('Poco F5','Lumayan Terkenal',1134558,'5000 mAh','5 jutaan',6.67),
+	 ('iQOO Z7','Kurang Terkenal',670439,'5000 mAh','4,3 jutaan',6.60),
+	 ('Redmi Note 12 Pro 5G','Sangat Terkenal',538000,'5000 mAh','4,3 jutaan',6.67),
+	 ('Oppo A78 5G','Cukup Terkenal',324000,'5000 mAh','4 jutaan',6.56),
+	 ('Realme GT Neo 3T','Lumayan Terkenal',789376,'5000 mAh','5,5 jutaan',6.62);
